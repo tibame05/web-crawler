d99d01ff15d31273fc59e4bba0027d12ef33d344
joyceHsu65 <egroup.joyce@gmail.com>
2025-06-13 01:41:27 +0800

    update

--- etf_analysis_cleaned.py
+++ etf_analysis_cleaned.py
@@ -2,14 +2,19 @@
 import mplfinance as mpf
 import pandas as pd
 import yfinance as yf
+import os
 
-# 讀取上傳的 ETF CSV（預期包含 '產品名稱' 欄位為 ticker）
-etf_df = pd.read_csv("/Users/joycehsu/Downloads/etf_list.csv")
+output_dir = "output"
+os.makedirs(output_dir, exist_ok=True)
+
+# 讀取上傳的 ETF CSV（預期包含 '序號' 欄位為 ticker）
+etf_df = pd.read_csv("output/etf_list.csv", encoding="utf-8-sig", sep="\t")
+etf_df.columns = etf_df.columns.str.strip()
 ticker_list = etf_df["序號"].dropna().tolist()
 
-# 下載台積電從 2023-01-01 到今天的股價資料
+# 下載各ETF從 2015-01-01 到今天的股價資料
 for ticker in ticker_list:
     start_date = '2015-01-01'
     end_date = pd.Timestamp.today().strftime('%Y-%m-%d')
     df = yf.download(ticker, start=start_date, end=end_date,multi_level_index = False, auto_adjust= False )
-    df.to_csv(f"/output/{ticker}.csv")
+    df.to_csv(f"{output_dir}/{ticker}.csv")
